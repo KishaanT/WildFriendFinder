286a93b496784acd177e29d24f8c5c2ab7f97afa
KishaanT <kishaan2006@gmail.com>
2025-04-16 21:09:40 -0400

    homepage

---
+++ wildfriendfinder/lib/homePage.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'listingPage.dart';
+
+class homePage extends StatefulWidget {
+  const homePage({super.key});
+
+  @override
+  State<homePage> createState() => _homePageState();
+}
+
+class _homePageState extends State<homePage> {
+  int _selectIndex = 0;
+  List<StatefulWidget> _widgetOption = [
+    homePage(),
+    listingPage()
+  ];
+
+  void pageChange(int index){
+    setState(() {
+      _selectIndex = index;
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Wild Friend Finder",style: TextStyle(color: Colors.deepPurple),),
+      ),
+      body: Center(
+        child: Text("Home Page",style: TextStyle(fontSize: 35,fontWeight: FontWeight.bold),)
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.home),label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.list),label: "List")
+          ],
+        type: BottomNavigationBarType.shifting,
+        currentIndex: _selectIndex,
+        selectedItemColor: Colors.indigoAccent,
+        onTap: pageChange,
+        iconSize: 35,
+
+      ),
+    );
+  }
+}
